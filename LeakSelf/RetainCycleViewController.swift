1f86eaffcc221dffb1642356ea491774bd7d743b
Adriano Bonat <adrianob@gmail.com>
2017-05-26 11:55:27 -0300

    Add solutions to retain cycle

--- LeakSelf/RetainCycleViewController.swift
+++ LeakSelf/RetainCycleViewController.swift
@@ -5,10 +5,19 @@ class RetainCycleViewController: UIViewController {
     var doDismissCompletion: (() -> Void)?
 
     @IBAction func backTouchUp() {
-        self.doDismissCompletion = { _ in
+        // Solution #1: capture self weakly
+        self.doDismissCompletion = { /*[weak self]*/ _ in
             self.cleanUp()
+            //self?.cleanUp()
         }
         dismiss(animated: true, completion: self.doDismissCompletion)
+
+        // Solution #2: comment previous code and uncomment the following line
+        //dismiss(animated: true, completion: self.doDismissCompletionWithFunc)
+    }
+
+    func doDismissCompletionWithFunc() {
+        self.cleanUp()
     }
 
     func cleanUp() {
